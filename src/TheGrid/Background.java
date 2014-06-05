c3b48ba72265a7f152ad78c7ec6414a6dd7c10a4
wkuglen <wkuglen@gmail.com>
2014-06-05 18:27:13 -0500

    Documentation of The Grid system. Removed class files from future commits

--- src/TheGrid/Background.java
+++ src/TheGrid/Background.java
@@ -2,10 +2,17 @@ package TheGrid;
 
 import info.gridworld.actor.Rock;
 
+/**
+ * @author Will Kuglen
+ * Background is a Wrapper for Rock
+ */
 public class Background extends Rock {
-
+	
+	/**
+	 * Default constructor - Creates a rock; Basically gives a different look to the rock
+	 */
 	public Background()
 	{
 		super();
-	}
-}
+	}//public Background()
+}//public class Background extends Rock
--- src/TheGrid/Trail.java
+++ src/TheGrid/Trail.java
@@ -1,16 +1,20 @@
 package TheGrid;
 
-import info.gridworld.actor.Actor;
 import info.gridworld.actor.Rock;
-import info.gridworld.grid.Grid;
-import info.gridworld.grid.Location;
-
 import java.awt.Color;
 
+/**
+ * @author Will Kuglen
+ * Trail is a Colored Wrapper class for a Rock
+ */
 public class Trail extends Rock{
 	
+	/**
+	 * Default constructor - Creates a rock; Basically gives a different look and color to the rock
+	 * @param c intended Color of Trail, should be the same as the Bike
+	 */
 	public Trail(Color c)
 	{
 		super(c);
-	}
-}
+	}//public Trail(Color c)
+}//public class Trail extends Rock
